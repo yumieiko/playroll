aa843dff8cecb69b067296ca6c0a5b473cf36bc9
yumieiko <r3s1zetv@gmail.com>
2025-06-25 07:32:33 -0700

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,15 @@
 #include "pwe_parser/PWEParser.h"
 
 int main() {
-    PWEParser pweParser;
-    pweParser.pack("C:\\Users\\sol\\CLionProjects\\windows-pwe\\pwe_playbook",
-                   "C:\\Users\\sol\\CLionProjects\\windows-pwe\\playbook.pwe");
-    pweParser.unpack("C:\\Users\\sol\\CLionProjects\\windows-pwe\\playbook.pwe",
-                     "C:\\Users\\sol\\CLionProjects\\windows-pwe\\uvunpackked");
-
-    YAML::Node config = YAML::LoadFile("C:\\Users\\sol\\CLionProjects\\windows-pwe\\uv.yaml");
-    for (auto steps : config["steps"]) {
-        std::cout << steps.first << std::endl;
-    }
+    // PWEParser pweParser;
+    // pweParser.pack("C:\\Users\\sol\\CLionProjects\\windows-pwe\\pwe_playbook",
+    //                "C:\\Users\\sol\\CLionProjects\\windows-pwe\\playbook.pwe");
+    // pweParser.unpack("C:\\Users\\sol\\CLionProjects\\windows-pwe\\playbook.pwe",
+    //                  "C:\\Users\\sol\\CLionProjects\\windows-pwe\\uvunpackked");
+    //
+    // YAML::Node config = YAML::LoadFile("C:\\Users\\sol\\CLionProjects\\windows-pwe\\uv.yaml");
+    // for (auto steps : config["steps"]) {
+    //     std::cout << steps.first << std::endl;
+    // }
     return 0;
 }
